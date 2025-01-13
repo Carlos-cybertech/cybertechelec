7d1b1d37bfafdfcb6c24b42a380e991a6883b9e4
CarlosCruzValverde <carlosczvalverde@gmail.com>
2025-01-13 13:30:50 -0800

    modify gunicorn_config

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,5 +1,3 @@
-import os
-
-workers = 2  # Number of worker processes
-threads = 4  # Number of threads per worker
-bind = f"0.0.0.0:{os.environ.get('PORT', 8000)}"  # Bind to the port set in the environment variable or default to 8000
+workers = 2
+threads = 4
+bind = "0.0.0.0:8000"
